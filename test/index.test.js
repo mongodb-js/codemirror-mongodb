7cc2607c71cb7f51e418a9267da77f9ed467388c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2017-03-22 09:55:51 -0400

    [ci skip] gh-pages deploy

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-var codemirrorMongodb = require('../');
-var assert = require('assert');
-
-describe('codemirror-mongodb', function() {
-  it('should work', function() {
-    assert(codemirrorMongodb);
-  });
-});
