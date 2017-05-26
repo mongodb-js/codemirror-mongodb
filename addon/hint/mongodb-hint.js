63ec329fd621312c4153bea9a25f744b7be2c8ad
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2017-05-25 21:02:14 -0400

    More refactoring for cleaner Compass integration

--- addon/hint/mongodb-hint.js
+++ addon/hint/mongodb-hint.js
@@ -1,9 +1,8 @@
-var CodeMirror = require('codemirror');
-var createPosition = CodeMirror.Pos;
+const CodeMirror = require('codemirror');
+const createPosition = CodeMirror.Pos;
+const debug = require('debug')('codemirror-mongodb:mongodb-hint');
 
-var debug = require('debug')('codemirror-mongodb:mongodb-hint');
-
-var OPERATORS = [
+const OPERATORS = [
   '$gte',
   '$gt',
   '$lte',
@@ -18,9 +17,118 @@ var OPERATORS = [
   '$all'
 ];
 
-function MongoDBHintProvider(opts) {
-  this.dbs = opts.dbs || {};
-  this.fields = opts.fields || { _id: 'ObjectId' };
+function splice(base, index, s) {
+  return base.substring(0, index) + s + base.substring(index, base.length);
+}
+
+class MongoDBHintProvider {
+  constructor(fields = { _id: 'ObjectId' }) {
+    this.fields = fields;
+
+    Object.keys(this.fields).forEach(k => {
+      if (typeof k === 'object') return;
+      if (typeof k === 'string') {
+        const fieldType = this.fields[k];
+
+        this.fields[k] = {
+          name: k,
+          path: k,
+          type: fieldType
+        };
+      }
+    });
+  }
+
+  execute(cm) {
+    const cursor = cm.getCursor();
+    let token = cm.getTokenAt(cursor);
+
+    if (!/^[\w$_]*$/.test(token.string)) {
+      debug('not a word-style token, ignore the token.', token);
+      token = {
+        isWordStyle: false,
+        start: cursor.ch,
+        end: cursor.ch,
+        string: '',
+        state: token.state,
+        type: token.string === '.' ? 'property' : null
+      };
+    } else if (token.end > cursor.ch) {
+      token.end = cursor.ch;
+      token.string = token.string.slice(0, cursor.ch - token.start);
+    }
+
+    const inputWhenTriggered = `${splice(cm.getValue(), cursor.ch, '|')}`;
+
+    const completions = [];
+
+    /**
+     * Case: List Field Names or Operators With Prefix
+     */
+    if (token.type === 'variable') {
+      debug(
+        '%s -> show only field names starting with `%s`',
+        inputWhenTriggered,
+        token.string
+      );
+
+      if (token.string.charAt(0) === '$') {
+        const PREFIX = new RegExp('^\\' + token.string);
+        completions.push.apply(
+          completions,
+          OPERATORS.filter(function(o) {
+            return PREFIX.test(o);
+          })
+        );
+      } else {
+        const PREFIX_RE = new RegExp('^' + token.string);
+        completions.push.apply(
+          completions,
+          Object.keys(this.fields).filter(function(k) {
+            return PREFIX_RE.test(k);
+          })
+        );
+      }
+      /**
+       * TODO (imlucas) Automatically start the next clause?
+       * if (completions.length === 1) ->
+       *  completions.push(completions[0] + ': ')
+       */
+    } else if (token.state.cc.length === 2) {
+      /**
+       * Case: Blank Slate
+       */
+      debug('%s -> show field names only', inputWhenTriggered);
+      completions.push.apply(completions, Object.keys(this.fields));
+    } else if (token.state.cc.length === 5) {
+      debug('%s -> show field names only', inputWhenTriggered);
+      completions.push.apply(completions, Object.keys(this.fields));
+    } else if (token.state.cc.length === 4) {
+      debug('%s -> show list of operators', inputWhenTriggered);
+      completions.push.apply(completions, OPERATORS);
+    } else {
+      /**
+       * TODO (imlucas)
+       * if inputWhenTriggered = `{_id: |}` and
+       * the schema type of _id is an ObjectId ->
+       *  completions.push('ObjectId("|")')
+       */
+      debug('%s -> expect user to input a value', inputWhenTriggered);
+    }
+
+    if (completions.length > 0) {
+      debug('completions', completions);
+    }
+
+    /**
+     * TODO (imlucas) For completions with `.`s, text replacement should be wrapped in single quotes.
+     */
+    return {
+      list: completions,
+      from: createPosition(cursor.line, token.start),
+      to: createPosition(cursor.line, token.end)
+    };
+  }
 }
 
 // MongoDBHintProvider.prototype.gatherFields = function(prefix) {
@@ -61,106 +169,16 @@ function MongoDBHintProvider(opts) {
 //   });
 // };
 
-function splice(base, index, s) {
-  return base.substring(0, index) + s + base.substring(index, base.length);
-}
-
-MongoDBHintProvider.prototype.execute = function(cm) {
-  var cursor = cm.getCursor();
-  var token = cm.getTokenAt(cursor);
-
-  if (!/^[\w$_]*$/.test(token.string)) {
-    debug('not a word-style token, ignore the token.', token);
-    token = {
-      isWordStyle: false,
-      start: cursor.ch,
-      end: cursor.ch,
-      string: '',
-      state: token.state,
-      type: token.string === '.' ? 'property' : null
-    };
-  } else if (token.end > cursor.ch) {
-    token.end = cursor.ch;
-    token.string = token.string.slice(0, cursor.ch - token.start);
-  }
-
-  var inputWhenTriggered = `${splice(cm.getValue(), cursor.ch, '|')}`;
-  var completions = [];
-  /**
-   * Case: List Field Names or Operators With Prefix
-   */
-  if (token.type === 'variable') {
-    console.log(
-      '%s -> show only field names starting with `%s`',
-      inputWhenTriggered,
-      token.string
-    );
-
-    if (token.string.charAt(0) === '$') {
-      var PREFIX = new RegExp('^\\' + token.string);
-      completions.push.apply(
-        completions,
-        OPERATORS.filter(function(o) {
-          return PREFIX.test(o);
-        })
-      );
-    } else {
-      var PREFIX_RE = new RegExp('^' + token.string);
-      completions.push.apply(
-        completions,
-        Object.keys(this.fields).filter(function(k) {
-          return PREFIX_RE.test(k);
-        })
-      );
-    }
-    /**
-     * TODO (imlucas) Automatically start the next clause?
-     * if (completions.length === 1) ->
-     *  completions.push(completions[0] + ': ')
-     */
-  } else if (token.state.cc.length === 2) {
-    /**
-     * Case: Blank Slate
-     */
-    console.log('%s -> show field names only', inputWhenTriggered);
-    completions.push.apply(completions, Object.keys(this.fields));
-  } else if (token.state.cc.length === 5) {
-    console.log('%s -> show field names only', inputWhenTriggered);
-    completions.push.apply(completions, Object.keys(this.fields));
-  } else if (token.state.cc.length === 4) {
-    console.log('%s -> show list of operators', inputWhenTriggered);
-    completions.push.apply(completions, OPERATORS);
-  } else {
-    /**
-     * TODO (imlucas)
-     * if inputWhenTriggered = `{_id: |}` and
-     * the schema type of _id is an ObjectId ->
-     *  completions.push('ObjectId("|")')
-     */
-    console.log('%s -> expect user to input a value', inputWhenTriggered);
-  }
-
-  var data = {
-    list: completions,
-    from: createPosition(cursor.line, token.start),
-    to: createPosition(cursor.line, token.end)
-  };
-
-  console.log('providing', data);
-  return data;
-};
-
 module.exports = MongoDBHintProvider;
 
-// Override autocomplete command to point at our provider.
+function showMongoDBHintsForEditor(editor) {
+  const opts = editor.options.hintOptions.mongodb;
+  const hinter = new MongoDBHintProvider(opts.fields);
+  return hinter.execute(editor);
+}
+
 CodeMirror.commands.autocomplete = function(cm) {
-  CodeMirror.showHint(cm, function(editor) {
-    var opts = editor.options.hintOptions.mongodb;
-    var hinter = new MongoDBHintProvider(opts);
-    return hinter.execute(editor);
-  });
+  CodeMirror.showHint(cm, showMongoDBHintsForEditor);
 };
 
-CodeMirror.commands.parse = function(cm) {
-  debugger;
-};
+CodeMirror.commands.parse = function(cm) {};
